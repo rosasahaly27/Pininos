48066ec27223bcc5b3756cc5006f08b230614ad0
scarcharly_@hotmail.com <scarcharly_@hotmail.com>
2020-04-17 14:08:42 -0500

    solucion

---
+++ CarlProject/CarlProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CarlProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
