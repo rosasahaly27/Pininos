509ec6315a1e34b6ded9bf4ea15387671b4f03c3
scarcharly_@hotmail.com <scarcharly_@hotmail.com>
2020-04-17 15:35:27 -0500

    int

--- CarlProject/CarlProject/Program.cs
+++ CarlProject/CarlProject/Program.cs
@@ -8,11 +8,22 @@ namespace CarlProject
         {
 
             string entrada;
-            int A;
+            int A,B;
+            int r;
 
             Console.WriteLine("pon el valor de  A");
             entrada = Console.ReadLine();
+            A = Convert.ToInt32(entrada);
+            Console.WriteLine("pon el valor de B ");
+            entrada = Console.ReadLine();
+            B = Convert.ToInt32(entrada);
+            r = A + B;
+            
+
+            Console.WriteLine("el resultado de:"+ r);
+            Console.WriteLine("precione enter para continuar");
+            entrada = Console.ReadLine();
 
         }
-    }
+    }           
 }
