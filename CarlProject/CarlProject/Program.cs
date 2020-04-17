4b9303b7d0808c38590f8d09efd645603876bf66
scarcharly_@hotmail.com <scarcharly_@hotmail.com>
2020-04-17 14:12:29 -0500

    prueba de cambios

--- CarlProject/CarlProject/Program.cs
+++ CarlProject/CarlProject/Program.cs
@@ -7,7 +7,7 @@ namespace CarlProject
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Prueba de cambios!");
         }
     }
 }
