ac20916ee537d45378524b72ac497bafe95d2eaa
scarcharly_@hotmail.com <scarcharly_@hotmail.com>
2020-04-22 18:51:46 -0500

    edades

--- CarlProject/CarlProject/Program.cs
+++ CarlProject/CarlProject/Program.cs
@@ -8,22 +8,55 @@ namespace CarlProject
         {
 
             string entrada;
-            int A,B;
-            int r;
-
-            Console.WriteLine("pon el valor de  A");
-            entrada = Console.ReadLine();
-            A = Convert.ToInt32(entrada);
-            Console.WriteLine("pon el valor de B ");
-            entrada = Console.ReadLine();
-            B = Convert.ToInt32(entrada);
-            r = A + B;
-            
-
-            Console.WriteLine("el resultado de:"+ r);
-            Console.WriteLine("precione enter para continuar");
-            entrada = Console.ReadLine();
-
+            int edad;
+            do
+            {
+                Console.WriteLine("favor de ingresar tu edad");
+                entrada = Console.ReadLine();
+                edad = Convert.ToInt32(entrada);
+                NewMethod(edad);
+                Console.WriteLine("Preciona G para continuar");
+                entrada = Console.ReadLine();
+            } while (entrada != "g");
         }
-    }           
-}
+
+        private static void NewMethod(int edad)
+        {
+            if (edad >= 0 && edad <= 3)
+            {
+                Console.WriteLine("infante");
+            }
+            else if (edad >= 4 && edad <= 11)
+            {
+                Console.WriteLine("niño");
+            }
+            else if (edad >= 12 && edad <= 15)
+            {
+                Console.WriteLine(" pubertad");
+            }
+            else if (edad >= 16 && edad < 20)
+            {
+                Console.WriteLine("adolecencia");
+
+            }
+            else if (edad >= 21 && edad <= 35)
+            {
+                Console.WriteLine("juventud");
+            }
+            else if (edad >= 36 && edad <= 50)
+            {
+                Console.WriteLine("adulto");
+
+            }
+            else if (edad >= 50)
+            {
+                Console.WriteLine("adulto mayor");
+            }
+            else
+            {}
+        }
+    }
+    }
+
+
+
